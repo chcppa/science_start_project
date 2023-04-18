2836f15ecf58e051b64dc7dda4f655c3c1f7ba81
Kuwerin <45897893+Kuwerin@users.noreply.github.com>
2023-04-18 16:00:14 +0300

    Update project structure and add docker support
    * Remove unnecessary level in filestructure
    
    * Add docker-compose and Dockerfile
    
    * Delete JetBrains-specific stuff

--- src/orm/database.py
+++ src/orm/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker, Session as SessionType
 
 from settings import settings
 
-db = f"{settings.db_url}chcppa:ZMBa83sYJ2xm9fMk5FwMnh4lWGqhaA22@dpg-cgume7539el96k1vca70-a.frankfurt-postgres.render.com/database_45be" #f"{settings.db_url}{settings.db_user}:{settings.db_password}@{settings.db_host}/{settings.db_name}"
+db = f"postgresql+psycopg2://{settings.db_user}:{settings.db_password}@{settings.db_host}/{settings.db_name}"
 engine = create_engine(db, echo=False)
 Session = sessionmaker(bind=engine, autocommit=False)
 
